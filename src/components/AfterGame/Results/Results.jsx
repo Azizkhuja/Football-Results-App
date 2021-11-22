cd5f32d0f92040ffa367dff443b0ac86e20c6685
Aziz <aziz@Azizs-MacBook-Pro.local>
2021-11-22 11:37:30 +0100

    top scorers data retrving correctly

--- src/components/AfterGame/Results/Results.jsx
+++ src/components/AfterGame/Results/Results.jsx
@@ -1,10 +1,23 @@
 import React from "react";
-
+import {
+  Grid,
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+  CardActionArea,
+  Button,
+} from "@mui/material";
 const Results = () => {
   return (
-    <div>
-      <h1>Results</h1>
-    </div>
+    <Grid container mt={2}>
+      <Grid item xs={6}>
+        <Typography variant="h5">Results</Typography>
+      </Grid>
+      <Grid item xs={6} sx={{ display: "flex", justifyContent: "flex-end" }}>
+        <Button variant="text">View all</Button>
+      </Grid>
+    </Grid>
   );
 };
 
